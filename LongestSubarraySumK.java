913644bff216d01d0cd708db02881ec76eca73ce
Sumit Kumar <145086126+Sumit-22@users.noreply.github.com>
2025-08-20 20:22:06 +0000

    Initial commit

--- LongestSubarraySumK.java
+++ LongestSubarraySumK.java
@@ -1,24 +1,16 @@
-class LongestSubarraySumK {//only for +ve
+class LongestSubarraySumK {
     public static int longestSubarray(int[] arr, int k) {
-        int i = 0, j = 0, sum = 0, maxLen = 0;
+        int i = 0, sum = 0, maxLen = 0;
 
-        while (j < arr.length) {
+        for (int j = 0; j < arr.length; j++) {
             sum += arr[j];
 
             while (sum > k) {
                 sum -= arr[i];
                 i++;
             }
-
             if (sum == k) maxLen = Math.max(maxLen, j - i + 1);
-
-            j++;
         }
         return maxLen;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {4,1,1,1,2,3,5};
-        System.out.println(longestSubarray(arr, 5));
     }
 }
