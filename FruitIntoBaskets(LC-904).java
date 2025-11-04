cb8434f16f95a70275aa215c3d7feb0d341a3b2f
Sumit Kumar <145086126+Sumit-22@users.noreply.github.com>
2025-11-04 18:21:59 +0000

    commit

--- FruitIntoBaskets(LC-904).java
+++ FruitIntoBaskets(LC-904).java
@@ -1,3 +1,8 @@
+/*
+ * LeetCode 904 – Fruit Into Baskets
+ * Goal: Find the longest subarray containing at most 2 distinct integers.
+ */
+
 import java.util.*;
 
 class Solution904 {
@@ -22,3 +27,29 @@ class Solution904 {
         return maxLen;
     }
 }
+/*
+ * LeetCode 1004 – Max Consecutive Ones III
+ * Goal: You can flip at most k zeros to 1. Find the longest subarray with only 1s after flips.
+ * 
+ */
+class Solution1004 {
+    public int longestOnes(int[] nums, int k) {
+        int left = 0, zeros = 0, maxLen = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) zeros++;
+
+            // If more than k zeros, shrink window
+            while (zeros > k) {
+                if (nums[left] == 0) zeros--;
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+}
+ //Complexity:
+ //Time O(n), Space O(1)
