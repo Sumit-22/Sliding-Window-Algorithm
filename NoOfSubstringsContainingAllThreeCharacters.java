9eb3c47088499550875450c233fc8016fa87f26b
Sumit Kumar <145086126+Sumit-22@users.noreply.github.com>
2025-11-04 12:07:07 +0000

    commit

--- NoOfSubstringsContainingAllThreeCharacters.java
+++ NoOfSubstringsContainingAllThreeCharacters.java
@@ -25,3 +25,20 @@ Keep track of last index where each character 'a', 'b', 'c' was seen.
 
 Once all three seen, every substring ending at current index and starting before min(lastSeen) is valid.
  */
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int [] count = new int[3];
+        int left =0;
+        int result  =0;
+        for(int i=0; i< s.length();i++){
+            count[s.charAt(i)-'a']++;
+
+            while(count[0] > 0 && count[1] > 0 && count[2] > 0){
+                result += s.length()-i;
+                count[s.charAt(left)-'a']--;
+                left++; 
+            }
+        }
+        return result;
+    }
+}
