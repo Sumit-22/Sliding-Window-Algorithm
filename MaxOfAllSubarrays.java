1a71ba2abcc5f50c4a0011c3aa8098f2d90d7290
Sumit Kumar <145086126+Sumit-22@users.noreply.github.com>
2025-11-05 06:53:12 +0000

    commit

--- MaxOfAllSubarrays.java
+++ MaxOfAllSubarrays.java
@@ -19,3 +19,33 @@ class MaxOfAllSubarrays {
         return ans;
     }
 }
+
+// or other readable code but with same algo
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> dq = new ArrayDeque<>(); // store indices
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        int idx = 0;
+
+        for (int i = 0; i < n; i++) {
+            // 1️⃣ Remove elements out of the current window
+            while (!dq.isEmpty() && dq.peekFirst() <= i - k)
+                dq.pollFirst();
+
+            // 2️⃣ Remove smaller elements (they can’t be max anymore)
+            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()])
+                dq.pollLast();
+
+            // 3️⃣ Add current element’s index
+            dq.offerLast(i);
+
+            // 4️⃣ Store max once the window is of size k
+            if (i >= k - 1)
+                res[idx++] = nums[dq.peekFirst()];
+        }
+
+        return res;
+    }
+}
+
