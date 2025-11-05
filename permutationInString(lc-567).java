09767d511237d74b09cec02b356f653d7b48d8ff
Sumit Kumar <145086126+Sumit-22@users.noreply.github.com>
2025-11-05 14:08:31 +0000

    message

--- permutationInString(lc-567).java
+++ permutationInString(lc-567).java
@@ -16,9 +16,18 @@ public class solution{
 
     int left = 0, right = 0, match = s1.length();
     while (right < s2.length()) {
-        if (count[s2.charAt(right++) - 'a']-- > 0) match--;
+        int idx = s2.charAt(right++) - 'a';
+        if (count[idx]-- > 0) match--;
         if (match == 0) return true;
         if (right - left == s1.length() && count[s2.charAt(left++) - 'a']++ >= 0) match++;
+  // or 
+  /*  if ((right - left) == s1.length()) {
+            int leftIdx = s2.charAt(left++) - 'a';
+            if (count[leftIdx] >= 0) {
+                match++;
+            }
+            count[leftIdx]++;
+        }*/
     }
     return false;
 }
